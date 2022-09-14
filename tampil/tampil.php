97f83539347f58ceabbbd32183a808a024ad1f36
fajarsiddiq16 <113009101+fajarsiddiq16@users.noreply.github.com>
2022-09-14 00:36:42 -0700

    Update tampil.php

--- tampil/tampil.php
+++ tampil/tampil.php
@@ -1,2 +1,31 @@
-<1DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hasil Login</title>
+    <link rel-"stylesheet" href-"style.css" type-"text/css">
+  </head>
+  <body>
+    
+ <?php
+$nama = $_POST["username"]:
+$email = $_POST["email"]:   
+    
+?>
+<form action-"tampil.php" method="POST">
+  <h2>Selamat Anda Berhasil login </h2>
+  <div class-"imgcontainer">
+    <img src-"icon.png" alt-"Avatar" class"avatar">
+  </div>
   
+  <div class-"container">
+    <label><b>Username : <7php echo $nama;7></b></
+      label><br><br>
+      
+      <label><b>Password : <7php echo $email;7></b></label>
+        </div>
+      
+      <div class-"container" style-"background-color:#flflfl">
+      </div>
+      </form>
+    </body>
+  </html>          
