1baf8069bf4e84c2b08b20f8ce1db74d86ef42b5
fajarsiddiq16 <113009101+fajarsiddiq16@users.noreply.github.com>
2022-09-14 00:14:05 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<1DOCTYPE html> 
+<!DOCTYPE html> 
+<html>
+<head>
+  <title>Login Form</title>
+  <link rel-"stylesheet" href-"style.css" type-"text/css">
+</head>
+<body>
+
+<!-- <h2>login Form </h2> -->
+
+<form action-"tampil.php" method-"POST">
+  <h2>Form Login <h/2>
+  <div class-"imgcontainer">
+      <img src-"icon.png" alt-"Avatar" class-"avatar">
+  </div>
+    
+  <div class-"container">
+    <label><b>Username</b></label>
+     <input type-"text" placeholder-"Masukan Username"
+            name-"username" required>
+    
+    <label><b>Password</b></label>
+     <input type-"password" placeholder-"Masukan Password"
+            name-"email" required>
+    <button type-"submit">Masuk</button>
+    <input type-"checkbox" checked-"checked"><span>Ingat Saya</span>
+    </div>
+    
+    <div class-"container" style-"background-color:#flflfl">
+      <button type-"button" class-"cancelbtn">Batal</button>
+      <span class-"psw">Lupa <a href-"#">password?</a></span>
+    </div>
+    </form>
+    
+  </body>
+  </html>
+  
